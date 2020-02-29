44b035a9d9396a99f9560431d357fb525a95485f
植木孝史 <>
2020-02-29 23:49:51 +0900

    OK

---
+++ src/database/migrations/2020_02_29_214702_create_moves_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMovesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('moves', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('profile_id');
+            $table->string('move');
+            $table->timestamps();
+            
+            // 外部キーを追加
+            $table->foreign('profile_id')
+                ->references('id')
+                ->on('profiles')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('moves');
+    }
+}
